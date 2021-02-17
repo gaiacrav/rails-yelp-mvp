bb8d0cf8ec7b409b031ec99c6154912e4e97755a
Gaia Cravesana <gcravesana@gmail.com>
2021-02-17 13:14:31 +0000

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ilGelataio = Restaurant.create([{ name: "Il Gelataio" }, { address: "via nino bixio 7, milano" }, { category: "italian" }])
+myBurger = Restaurant.create([{ name: "My burger" }, { address: "Avenida da liberdade, Lisboa" }, { category: "belgian" }])
+sushiBox = Restaurant.create([{ name: "Sushi Box" }, { address: "Saldanha, Lisboa" }, { category: "japanese" }])
+pechinoExpress = Restaurant.create([{ name: "Pechino Express" }, { address: "viale lombardia, Milano" }, { category: "chinese" }])
+bijoux = Restaurant.create([{ name: "Bijoux" }, { address: "Champs Elysee, Paris" }, { category: "french" }])
